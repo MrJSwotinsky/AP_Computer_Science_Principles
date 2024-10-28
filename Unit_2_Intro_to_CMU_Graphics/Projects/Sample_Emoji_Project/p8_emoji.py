c3100e84f9465f8aab4e199e9e369bf4d301476f
MrJSwotinsky <jswotinsky@schools.nyc.gov>
2024-10-28 15:48:23 -0400

    Updated based on feedback.
    Yanizmarie suggested pink.  Melanie suggest gradient

--- Unit_2_Intro_to_CMU_Graphics/Projects/Sample_Emoji_Project/p8_emoji.py
+++ Unit_2_Intro_to_CMU_Graphics/Projects/Sample_Emoji_Project/p8_emoji.py
@@ -1,8 +1,8 @@
 # Import everythin I need from CMU graphics:
 from cmu_graphics import *
 
-# Draw a big yellow circle in the center of the screen:
-Circle(200,200,200, fill='yellow')
+# Draw a big beautiful circle in the center of the screen:
+Circle(200,200,200, fill=gradient('orange','yellow',start='center'),border='pink',)
 
 # Run program:
 cmu_graphics.run()
