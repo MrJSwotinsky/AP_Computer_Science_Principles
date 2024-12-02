f13e99ae2a9bae8f64f31e64c1dea3770ebae112
knrobles <krobles6@schools.nyc.gov>
2024-12-02 11:36:51 -0500

    Add files via upload

---
+++ Resources/Custom_Shape_Properties_and_Functions.py
@@ -0,0 +1,39 @@
+from cmu_graphics import *
+
+'''
+player = Circle(200, 200, 50, fill='red')
+
+#Custom shape properties allow us to attach a variable to a shape
+
+#Write a program that adds energy to the player and turns it green when it is fully powered
+'''
+
+
+
+
+
+'''
+#hits(x,y) function returns a Boolean
+
+star = Star(200, 200, 140, 5, fill=None, borderWidth=15, border='lightGrey')
+l = Label('Move the mouse', 200, 350, size=30)
+
+#Write a program that changes the star border and label text when the mouse hits the star
+
+'''
+
+
+
+
+
+'''
+#hitsShape(shapeName) method returns a Boolean
+
+star = Star(200, 200, 140, 5, fill='lightGrey')
+l = Label('Move the mouse!', 200, 350, size=30)
+dot = Circle(200, 300, 20, fill='maroon')
+
+#Write a program that changes the star border and label text when the circle hits the star
+
+'''
+cmu_graphics.run()
